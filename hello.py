dc40fa0ee447e2c2374cfbb96512d4de7c812583
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2020-04-30 17:22:25 +0300

    Added support for sqlite database.

--- hello.py
+++ hello.py
@@ -19,14 +19,16 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-    users = db.relationship('User', backref = 'role')
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -42,24 +44,38 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators = [DataRequired()])
     submit = SubmitField('Submit')
 
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db = db, User = User, Role = Role)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data 
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'))
+    return render_template('index.html', form = form, name = session.get('name'),
+                           known = session.get('known', False))
 
 @app.route('/user/<name>')
 def user(name):
